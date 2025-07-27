2e4494f797cc44085c0d2f71ee2506e563ae19ba
aackun <sowhatmanuelle23@gmail.com>
2025-07-27 16:46:15 +0200

    sql template for assignment

---
+++ alu_student_performance.sql
@@ -0,0 +1,16 @@
+-- ALU Rwanda - Student Performance Database
+-- Coding Lab Group 22
+
+-- DROP TABLES IF EXISTS
+DROP TABLE IF EXISTS python_grades;
+DROP TABLE IF EXISTS linux_grades;
+DROP TABLE IF EXISTS students;
+
+-- CREATE TABLES
+-- ...
+
+-- INSERT SAMPLE DATA
+-- ...
+
+-- QUERIES
+-- ...
