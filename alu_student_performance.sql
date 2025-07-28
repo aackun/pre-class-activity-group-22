4242d1dbd755042650db6ced2a1dda6d6b026b70
Oluwatomi-Thompson <o.thompson@alustudent.com>
2025-07-28 07:51:10 +0200

    Created tables

--- alu_student_performance.sql
+++ alu_student_performance.sql
@@ -8,6 +8,26 @@ DROP TABLE IF EXISTS students;
 
 -- CREATE TABLES
 -- ...
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(100),
+    intake_year INT 
+);
+
+CREATE TABLE linux_grades (
+    course_id INT,
+    course_name VARCHAR(100),
+    student_id INT,
+    grade_obtained DECIMAL(5,2)
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+CREATE TABLE python_grades (
+    course_id INT,
+    course_name VARHCAR(100),
+    grade_obtained DECIMAL(5,2),
+    FOREIGN KEY (sutdent_id) REFERENCES students(student_id)
+)
 
 -- INSERT SAMPLE DATA
 -- ...
